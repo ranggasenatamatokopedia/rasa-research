8a1ec1d403d4cc712504459476481014d7876c61
Rangga <ranggasenatama@gmail.com>
2019-01-25 17:12:55 +0700

    Fix convert model rasa variant

---
+++ script/_format_csv_to_rasa_model_variant.py
@@ -0,0 +1,121 @@
+import csv
+import re
+import json
+
+def csv_file_reader_variant(directory):
+    text = {}
+    entities = {}
+    with open(directory) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            index = ""
+            word = ""
+            for (k, v) in row.items():
+                if k == "Group":
+                    try:
+                        entities[v].append(word)
+                    except:
+                        entities[v] = []
+                        entities[v].append(word)
+                    if v == "size":
+                        size_word = word.split(" ")[1]
+                        if index != word:
+                            size_index = index.split(" ")[1]
+                            try:
+                                text["size "+size_index].append(word)
+                            except:
+                                text["size "+size_index] = []
+                                text["size "+size_index].append(word)
+                            continue
+                        try:
+                            text["size "+size_word].append(word)
+                        except:
+                            text["size "+size_word] = []
+                            text["size "+size_word].append(word)
+                    else:
+                        if index != word:
+                            try:
+                                text[index].append(word)
+                            except:
+                                text[index] = []
+                                text[index].append(word)
+                if k == "Word":
+                    index = v.lower()
+                if k == "Typo":
+                    word = v.lower()
+    return text, entities
+
+def csv_file_reader_text(directory):
+    data = []
+    with open(directory, 'r', newline='', encoding='ISO-8859-1') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for (k, v) in row.items():
+                if k == "Text":
+                    data.append(v)
+    return data
+
+def rasa_model(text, data, entities):
+    rasa_json_format = {
+        "rasa_nlu_data": {
+            "common_examples": [],
+            "regex_features": [],
+            "lookup_tables": [],
+            "entity_synonyms": []
+        }
+    }
+    for value in text:
+        rasa_json_format["rasa_nlu_data"]["entity_synonyms"].append({"value": value,
+                                                                     "synonyms": [synm for synm in text[value]]})
+    for setence in data:
+        data_entity = []
+        start = 0
+        for entity in entities:
+            for word in entities[entity]:
+                try:
+                    start_index = re.search(word, setence[start:]).start()
+                    actual_value = word
+                    for value in text:
+                        if word == value:
+                            actual_value = value
+                            break
+                        else:
+                            for synm in text[value]:
+                                if word == synm:
+                                    actual_value = value
+                                    break
+                        if actual_value != word:
+                            break
+                    data_entity.append({
+                        "start": start_index,
+                        "end": start_index+len(word),
+                        "value": actual_value,
+                        "entity": entity
+                    })
+                    start = start_index+len(word)
+                except:
+                    continue
+        if len(data_entity) > 0:
+            rasa_json_format["rasa_nlu_data"]["common_examples"].append({"text": setence,
+                                                                        "intent": "ready_kosong_question",
+                                                                        "entities": data_entity})
+        else:
+            rasa_json_format["rasa_nlu_data"]["common_examples"].append({"text": setence,
+                                                                         "intent": "ready_kosong_question",
+                                                                         "entitites": []})
+
+    return rasa_json_format
+
+
+if __name__ == "__main__":
+    direct = "csv/string.csv"
+    direct_text = "csv/text_variant.csv"
+    text, entities = csv_file_reader_variant(direct)
+    for entity in entities:
+        entities[entity].sort(key=len, reverse= True)
+    data = csv_file_reader_text(direct_text)
+    # print(data)
+    # print(entities)
+    rasa = rasa_model(text, data, entities)
+    # print(rasa)
+    print(json.dumps(rasa, sort_keys=True, indent=4))
